531c20d0705e585dc2a8bb04b4278c8321a0563a
Naruto0613 <naranbadrakh1013@gmail.com>
2026-06-04 22:17:09 +0800

    slide

--- src/App.tsx
+++ src/App.tsx
@@ -3100,7 +3100,7 @@ export default function App() {
     <SolutionsSlide isActive={true} />,
     <SDGSlide isActive={true} />,
     <ActivitiesSlide isActive={true} />,
-    <SIASlide isActive={true} />,
+    <SIASlide isActive={false} />,
     <SIADetailsSlide isActive={true} />,
     <NationalAssemblySlide isActive={true} />,
     <SkillsSlide isActive={true} />,
